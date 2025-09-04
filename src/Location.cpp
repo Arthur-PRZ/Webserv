9040dd12436baa41d4dd79e9c73ed42f354557be
Clement Travers <clementtravers010@gmail.com>
2025-09-04 11:16:53 +0200

    feat: Location setter/getter

--- src/Location.cpp
+++ src/Location.cpp
@@ -1,20 +1,85 @@
 #include "Location.hpp"
 
-Location::Location(): _methods(""),  _root(""), _cgi_extensions(""), _uploads_path(""), _redirect(""), _autoindex(false) {}
+Location::Location()
+	: _methods(""),
+	  _root(""),
+	  _cgiExtensions(""),
+	  _uploadsPath(""),
+	  _redirect(""),
+	  _autoindex(false) {}
 
 Location::~Location() {}
 
-Location::Location(const Location& other): _methods(other._methods),  _root(other._root), _cgi_extensions(other._cgi_extensions), _uploads_path(other._uploads_path), _redirect(other._redirect), _autoindex(other._autoindex) {}
+Location::Location(const Location& other)
+	: _methods(other._methods),
+	  _root(other._root),
+	  _cgiExtensions(other._cgiExtensions),
+	  _uploadsPath(other._uploadsPath),
+	  _redirect(other._redirect),
+	  _autoindex(other._autoindex) {}
 
 Location &Location::operator=(const Location& other) {
 	if (this != &other)
 	{
 		_methods = other._methods;
 		_root = other._root;
-		_cgi_extensions = other._cgi_extensions;
-		_uploads_path = other._uploads_path;
+		_cgiExtensions = other._cgiExtensions;
+		_uploadsPath = other._uploadsPath;
 		_redirect = other._redirect;
 		_autoindex = other._autoindex;
 	}
 	return *this;
 }
+
+const std::string &Location::getMethods( void ) const {
+	return _methods;
+}
+
+const std::string &Location::getRoot( void ) const {
+	return _root;
+}
+
+
+const std::string &Location::getCGIExtensions( void ) const {
+	return _cgiExtensions;
+}
+
+
+const std::string &Location::getUploadsPath( void ) const {
+	return _uploadsPath;
+}
+
+
+const std::string &Location::getRedirect( void ) const {
+	return _redirect;
+}
+
+
+bool Location::getAutoIndex( void ) const {
+	return _autoindex;
+}
+
+void Location::setMethods( const std::string& methods) {
+	_methods = methods;
+}
+
+void Location::setRoot( const std::string& root) {
+	_root = root;
+}
+
+void Location::setCGIExtensions( const std::string& cgiExtensions) {
+	_cgiExtensions = cgiExtensions;
+}
+
+void Location::setUploadsPath( const std::string& uploadsPath) {
+	_uploadsPath = uploadsPath;
+}
+
+void Location::setRedirect( const std::string& redirect) {
+	_redirect = redirect;
+}
+
+void Location::setAutoIndex( const bool autoindex) {
+	_autoindex = autoindex;
+}
+
--- src/Location.hpp
+++ src/Location.hpp
@@ -1,11 +1,13 @@
-#include <iostream>
+#ifndef _LOCATION_HPP_ 
+#define _LOCATION_HPP_ 
+#include <string>
 
 class Location {
 	private:
 		std::string _methods;
 		std::string _root;
-	 	std::string _cgi_extensions;
-	 	std::string _uploads_path;
+	 	std::string _cgiExtensions;
+	 	std::string _uploadsPath;
 		std::string _redirect;
 		bool _autoindex;
 
@@ -14,4 +16,20 @@ class Location {
 		~Location();
 		Location(const Location& other);
 		Location &operator=(const Location& other);
+
+		const std::string &getMethods( void ) const;
+		const std::string &getRoot( void ) const;
+		const std::string &getCGIExtensions( void ) const;
+		const std::string &getUploadsPath( void ) const;
+		const std::string &getRedirect( void ) const;
+		bool getAutoIndex( void ) const;
+
+		void setMethods( const std::string& methods );
+		void setRoot( const std::string& root );
+		void setCGIExtensions( const std::string& cgiExtensions );
+		void setUploadsPath( const std::string& uploadsPath );
+		void setRedirect( const std::string& redirect);
+		void setAutoIndex( const bool autoindex );
 };
+
+#endif
