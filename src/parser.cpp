e3e68cb449e1b56bafb3412d3ac2f157c473bbf8
Arthur <arthur.perez66000@gmail.com>
2025-09-04 11:20:27 +0200

    parser good just need location

---
+++ src/parser.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Server.hpp"
+#include <map>
+
+void parser(std::ifstream &file, Server &server)
+{
+    std::string line;
+    size_t strSize = 0;
+    std::map<std::string, void(Server::*)(const std::string&)> setters;
+    setters["host"] = &Server::setHost;
+    setters["port"] = &Server::setPort;
+    setters["server_name"] = &Server::setServerName;
+    setters["root"] = &Server::setRoot;
+    setters["index"] = &Server::setIndex;
+    setters["error_page"] = &Server::setErrorPages;
+    setters["client_max_body_size"] = &Server::setClientMaxBodySize;
+
+
+    std::map<std::string, void(Server::*)(const std::string&)>::iterator curr;
+
+    while (std::getline(file, line))
+    {
+        for (curr = setters.begin(); curr != setters.end(); curr++)
+        {
+            if (line.find(curr->first) != std::string::npos)
+            {
+                strSize = line.find(curr->first);
+                std::string value = line.substr(strSize + curr->first.size() + 1);
+                value.pop_back();
+                (server.*(curr->second))(value);
+            }  
+        }
+    }
+}
+
+int main()
+{
+    Server server;
+
+    std::ifstream file("../config.conf");
+    parser(file, server);
+}
